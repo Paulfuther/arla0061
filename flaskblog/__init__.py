30d926aebb4ad1fd549992d4f993f910283b838b
Paul Futher <paul.futher@gmail.com>
2020-10-26 15:01:22 -0400

    no config file

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 #from flask_user import roles_required, UserManager, SQLAlchemyAdapter
 #from flaskblog import config
 import os
+import json
 from datetime import datetime
 
 from flask import render_template_string, url_for, redirect
@@ -26,12 +27,27 @@ from sqlalchemy import Boolean, DateTime, Column, Integer, \
     String, ForeignKey
 
 # Flask and Flask-SQLAlchemy initialization here
+#get variables
 
-#app = Flask(__name__)
-#app=app.config.from_object(config)
+
+#with open('/etc/config.json') as config_file:
+#	config = json.load(config_file)
+
+#app=app.config.from_object(config) --not needed
+
+#app.config['SECRET_KEY'] = config.get('SECRET_KEY')
+#app.config['SQLALCHEMY_DATABASE_URI'] = config.get('SQLALCHEMY_DATABASE_URI')
+#app.config['SECURITY_PASSWORD_SALT'] = config.get('SECURITY_PASSWORD_SALT')
+
+
+#delte next three lines on server
+
 app.config['SECRET_KEY'] = 'c164d8ed65cf46b1df5e336bd6adc4619a31830185f62b64'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SECURITY_PASSWORD_SALT'] = '6598120c4f17a416e33707393c85f809e782eb99f66a4527'
+
+#delte above three on server
+
 db = SQLAlchemy(app)
 #USER_APP_NAME ="app"
 
