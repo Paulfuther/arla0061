48e4af30e5a53a66041abb279226c32611865d49
Paul Futher <paul.futher@gmail.com>
2020-10-26 15:07:40 -0400

    git ignore

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -42,9 +42,7 @@ from sqlalchemy import Boolean, DateTime, Column, Integer, \
 
 #delte next three lines on server
 
-app.config['SECRET_KEY'] = 'c164d8ed65cf46b1df5e336bd6adc4619a31830185f62b64'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SECURITY_PASSWORD_SALT'] = '6598120c4f17a416e33707393c85f809e782eb99f66a4527'
+
 
 #delte above three on server
 
