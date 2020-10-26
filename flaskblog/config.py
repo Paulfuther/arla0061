e568f68c65d7275a7080298ab5efaf3505c9a935
paul futher <37282422+Paulfuther@users.noreply.github.com>
2020-10-26 15:15:57 -0400

    Add files via upload

---
+++ flaskblog/config.py
@@ -0,0 +1,16 @@
+
+
+
+
+    
+SECRET_KEY = 'mysecret'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+SECURITY_PASSWORD_SALT = 'none'
+
+
+app.config['SECRET_KEY'] = 'c164d8ed65cf46b1df5e336bd6adc4619a31830185f62b64'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECURITY_PASSWORD_SALT'] = '6598120c4f17a416e33707393c85f809e782eb99f66a4527'
+
+
+##one ModuleNotFoundError
