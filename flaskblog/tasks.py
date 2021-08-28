778afa20444716adb00972645f678fd8129531e2
paul futher <paulfuther@pauls-Mac-mini.local>
2021-08-28 17:28:49 -0400

    updates

--- flaskblog/tasks.py
+++ flaskblog/tasks.py
@@ -5,22 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#CELERY_BROKER_URL= os.environ.get('CELERY_BROKER_URL')
-#USR='codfish.rmq.cloudamqp.com'
-
-
 CLOUDAMQP_URL = os.environ.get('CLOUDAMQP_URL')
-
-#amp_authority = '(ampq_userinfo)@codfish.rmq.cloudamqp.com:5671/ftrnhyovk'
-#amp_userinfo = 'ftrhyovk:L-8g5-R5eGgBOzBPYAgKG1axKplnz4yy'
-#amqps_URI = 'ampqs://(amp_authority)'
 
 celery = Celery('tasks', broker=CLOUDAMQP_URL)
 
-#celery = Celery('tasks', broker='amqps://[USR]/ftrhyovk') 
-
-#celery = Celery('tasks', broker= 'amqps://ftrhyovk:L-8g5-R5eGgBOzBPYAgKG1axKplnz4yy@codfish.rmq.cloudamqp.com/ftrhyovk')
-
-#celery = Celery('tasks',broker= 'amqps://'AMPUSER:AMPPASSWORD'@codfish.rmq.cloudamqp.com/ftrhyovk')
-
-#celery = Celery('tasks', amqps_URI)
